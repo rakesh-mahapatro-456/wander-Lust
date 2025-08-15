895436a78509f2b65bad822490d5439980361220
Rakesh Mahapatro <rakeshmahapatro85@gmail.com>
2025-08-15 14:19:52 +0530

    updated show layout and listings

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://rakeshmahapatro85:5tb8yASrKyL66E5o@cluster0.stux7j7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
   .then(() => {
@@ -20,7 +20,7 @@ const initDB = async () => {
   await Listing.deleteMany({});
    initdata.data = initdata.data.map((obj) => ({
     ...obj,
-    owner: "6876824f40d20ebd7bdc0a8e",
+    owner: "689ee6da75c11ff9fffb91c8",
   }));
 
   await Listing.insertMany(initdata.data);
